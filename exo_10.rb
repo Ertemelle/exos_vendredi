bcfcd7e6dd6b4f4a435219c841eae99a33b54fad
leyssae <soualmia.farida@yahoo.fr>
2018-06-29 01:41:24 +0200

    code qui donne un age à partir d'une date entrée

---
+++ exo_10.rb
@@ -0,0 +1,6 @@
+puts "Quelle est votre date de naissance ?"
+print >
+date = gets.chomp.to_i
+puts "En 2017, vous aviez eu :" 
+Age = 2017 - date
+puts "Vous avez" + Age + "ans!! quel est votre secret?!"

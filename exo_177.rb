dce14809f4cf4a00008cf01fd22f5018bd5a2fdf
leyssae <40250700+leyssae@users.noreply.github.com>
2018-06-29 03:03:20 +0200

    Rename exo_17.rb to exo_177.rb

---
+++ exo_177.rb
@@ -0,0 +1,11 @@
+puts "quel est ton age?"
+print ">"
+age = gets.chomp.to_i
+x = 0
+y = 2017
+n= (y-x)/2
+while age < 2017
+x = x + 1
+if x = y 
+puts "il y a " + n +" ans tu avais la moitié de l'age que tu as aujourdhui"
+puts

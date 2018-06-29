4a4d72d864463a5d1dd0a06bf641898f95205fe9
leyssae <40250700+leyssae@users.noreply.github.com>
2018-06-29 02:23:40 +0200

    Create exo_13.rb

---
+++ exo_13.rb
@@ -0,0 +1,7 @@
+puts "Quelle est l'année de votre naissance?"
+print ">"
+année = gets.chomp.to_i
+
+while (année <2018)
+puts "Toujours là!!Merci de nous rappeler votre année de naissance!")
+année=année+1
